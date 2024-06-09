c6ba51473883d4130cc35861e2cdbb456b1523cb
Stephan Kugler (MAC) <stephankugler.stuff@gmail.com>
2024-06-09 20:47:41 +0200

    design the answer fields on the question cards

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// // Bookmark and Link Section
+
+// const homeLink = document.querySelector('[data-js="home.link"]');
+// const bookmarkLink = document.querySelector('[data-js="bookmark.link"]');
+// const userLink = document.querySelector('[data-js="user.link"]');
+// const homeSection = document.querySelector('[data-js="homeSection"]');
+// const bookmarkSection = document.querySelector('[data-js="bookmarkSection"]');
+// const userSection = document.querySelector('[data-js="userSection"]');
