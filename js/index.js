c2b0f56be149078794337c3656ed78f2063da87b
Stephan Kugler (MAC) <stephankugler.stuff@gmail.com>
2024-06-09 21:17:20 +0200

    add function to show answer buttons 1 and 2

--- js/index.js
+++ js/index.js
@@ -6,3 +6,32 @@
 // const homeSection = document.querySelector('[data-js="homeSection"]');
 // const bookmarkSection = document.querySelector('[data-js="bookmarkSection"]');
 // const userSection = document.querySelector('[data-js="userSection"]');
+
+//showAnswer Section
+const showAnswerButton1 = document.querySelector(
+    '[data-js="showAnswerButton1"]'
+);
+const showAnswerButton2 = document.querySelector(
+    '[data-js="showAnswerButton2"]'
+);
+const hiddenAnswer1 = document.querySelector('[data-js="answerQ01"]');
+const hiddenAnswer2 = document.querySelector('[data-js="answerQ02"]');
+
+showAnswerButton1.addEventListener('click', () => {
+    if (showAnswerButton1.innerText === 'Show Answer') {
+        showAnswerButton1.innerText = 'Hide Answer';
+        hiddenAnswer1.classList.toggle('hidden');
+    } else {
+        showAnswerButton1.innerText = 'Show Answer';
+        hiddenAnswer1.classList.toggle('hidden');
+    }
+});
+showAnswerButton2.addEventListener('click', () => {
+    if (showAnswerButton2.innerText === 'Show Answer') {
+        showAnswerButton2.innerText = 'Hide Answer';
+        hiddenAnswer2.classList.toggle('hidden');
+    } else {
+        showAnswerButton2.innerText = 'Show Answer';
+        hiddenAnswer2.classList.toggle('hidden');
+    }
+});
